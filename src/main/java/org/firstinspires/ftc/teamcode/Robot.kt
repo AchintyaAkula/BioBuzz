66615c94a0a3493c585cb81ec1e259bd7ae9ed6a
AchintyaAkula <achintya.akula@gmail.com>
2026-09-25 19:19:22 -0700

    Revert "need to delete"
    This reverts commit 63658b62241bdff5511fa3065f797198f6c42a9f.

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode
+
+import com.pedropathing.api.Paths.*;
+import com.pedropathing.paths.Path;
+import com.pedropathing.follower.Follower
+import org.firstinspires.ftc.teamcode.subsystems.Vision
+
+class Robot {
+    val follower: Follower by lazy { Constants.create(OpMode.hardwareMap) }
+    val vision: Vision = Vision()
+
+    fun goToNearestBlob() {
+        val target = vision.getTargetPose(follower.pose())
+        if (target == null) return
+
+        follower.follow(line(follower.pose(), target).tangent())
+    }
+}
--- src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.kt
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.pedro
-
-class Constants {
-}
